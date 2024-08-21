4431ad056e799e8e0180c74ae2af24c7ab5666c8
Maite Azcorra <azmaite@gmail.com>
2024-08-21 09:20:24 +0200

    Typo

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     ],
     author="Maite Azcorra",
     author_email="maite.azcorrasedano@epfl.ch",
-    description="Pipeline to pre-process simulanesouly recorded two-photon and behavioural data in flies.",
 
     long_description_content_type="text/x-rst",
     url="https://github.com/azcorra/imabeh",
