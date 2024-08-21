98220a7eeedbb732f8c6aaa8574efb0df6a9b857
Maite Azcorra <azmaite@gmail.com>
2024-08-21 11:54:23 +0200

    Added a util function to get the list of flies to process from a txt file, and a few tests to check it works properly

--- imabeh/run/runutils.py
+++ imabeh/run/runutils.py
@@ -3,13 +3,14 @@ utility functions to run processing using the TaskManager
 """
 
 import os
+import importlib
 
 from imabeh.run.userpaths import GLOBAL_PATHS
 
 
 def read_current_user(txt_file = GLOBAL_PATHS["txt_current_user"]):
     """
-    reads the supplied text file and returns the current user name.
+    Reads the supplied text file and returns the current user name.
     Format in the txt file:
     CURRENT_USER = USER_XXX
     Must match existing dictionary in 'run/userpaths.py'
@@ -50,7 +51,81 @@ def read_current_user(txt_file = GLOBAL_PATHS["txt_current_user"]):
     except ImportError:
         raise ValueError(f"User {current_user} does not exist in the userpaths.py file. Please create a dictionary for this user.")
 
-    return current_user
+    # get the current user specific settings
+    userpaths_module = importlib.import_module('imabeh.run.userpaths')
+    current_user_settings = getattr(userpaths_module, current_user)
+
+    return current_user_settings
+
+
+def read_fly_dirs(txt_file = GLOBAL_PATHS["txt_file_to_process"]):
+    """
+    reads the supplied text file and returns a list of dictionaries
+    with information for each fly to process and the tasks to run on it.
+    General requested format of a line in the txt file (see example in file):
+    fly_dir||trial1,trial2||task1,task2,!task3,
+    ! before a task forces an overwrite.
+    example:
+    date_genotype/Fly1||001_beh||fictrac,!df3d
+
+    Parameters
+    ----------
+    txt_file : str, optional
+        location of the text file, by default set in GLOBAL_PATHS["txt_file_to_process"]
+
+    Returns
+    -------
+    List[dict]
+        fly dict with the following fields:
+        - "dir": the base directory of the fly
+        - "selected_trials": a string describing which trials to run on,
+                             e.g. "001,002" or "all_trials"
+        - "tasks": a comma separated string containing the names of the tasks todo
+
+    """
+    # check that the file exists
+    if not os.path.exists(txt_file):
+        raise FileNotFoundError(f"File {txt_file} does not exist. Please create it with the flies to process.")
+    
+    # read file
+    with open(txt_file) as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+    fly_dicts = []
+
+    # get the flies to process
+    for line in lines:
+        if line.startswith("#") or line == "":
+            continue
+        strings = line.split("||")
+        # check that the line has the correct format
+        if len(strings) != 3:
+            raise ValueError(f"Line {line} does not have the correct format. Please use 'fly_dir||trial1,trial2||task1,task2,!task3,'")
+        # get the flies and tasks
+        fly = {
+            "dir": strings[0],
+            "selected_trials": strings[1],
+            "tasks": strings[2]
+        }
+        fly_dicts.append(fly)
+
+    # Check that the fly dirs exist
+    current_user_settings = read_current_user()
+    data_path = current_user_settings["labserver_data"]
+
+    for fly_dict in fly_dicts:
+        trial_path = os.path.join(data_path, fly_dict["dir"])
+        if not os.path.exists(trial_path):
+            raise FileNotFoundError(f"Fly directory {fly_dict['dir']} does not exist. Please check fly list {txt_file}.")
+        else:
+            # check that the trials exist
+            trials = fly_dict["selected_trials"].split(",")
+            for trial in trials:
+                trial_path = os.path.join(data_path, fly_dict["dir"], trial)
+                if not os.path.exists(trial_path):
+                    raise FileNotFoundError(f"Trial {trial} does not exist in fly directory {fly_dict['dir']}. Please check fly list {txt_file}.")
+
+    return fly_dicts
 
 
     
--- imabeh/run/runutils_tests.py
+++ imabeh/run/runutils_tests.py
@@ -5,19 +5,19 @@
 import unittest
 import os
 
-from imabeh.run.runutils import read_current_user
+from imabeh.run.runutils import read_current_user, read_fly_dirs
 
 
 class TestGetCurrentUser(unittest.TestCase):
     """ Test the runutils.read_current_user function. """
 
     def test_error_no_file(self):
-        """ Test that the function raises a FileNotFoundError if the file does not exist. """
+        """ _Test that the function raises a FileNotFoundError if the file does not exist. """
         with self.assertRaises(FileNotFoundError):
             read_current_user("nonexistent.txt")
 
     def test_error_no_current_user(self):  
-        """ Test that the function raises a ValueError if the file does not contain the CURRENT_USER variable. """
+        """ _Test that the function raises a ValueError if the file does not contain the CURRENT_USER variable. """
         with open("test.txt", "w") as file:
             file.write("USER_XXX")
         with self.assertRaises(ValueError):
@@ -25,12 +25,46 @@ class TestGetCurrentUser(unittest.TestCase):
         os.remove("test.txt")
 
     def test_error_no_userpaths(self):      
-        """ Test that the function raises a ValueError if the user does not exist in the userpaths file. """
+        """ _Test that the function raises a ValueError if the user does not exist in the userpaths file. """
         with open("test.txt", "w") as file:
             file.write("CURRENT_USER = USER_XXX")
         with self.assertRaises(ValueError):
             read_current_user("test.txt")
         os.remove("test.txt")
+
+class TestGetFlyDirs(unittest.TestCase):
+    """ Test the runutils.read_current_user function. """
+
+    def test_error_no_file(self):
+        """ _Test that the function raises a FileNotFoundError if the file does not exist. """
+        with self.assertRaises(FileNotFoundError):
+            read_fly_dirs("nonexistent.txt")
+
+    def test_error_wrong_format_short(self):  
+        """ _Test that the function raises a ValueError if the file list format is too short. """
+        with open("test.txt", "w") as file:
+            file.write("Fly_dir||Trial")
+        with self.assertRaises(ValueError):
+            read_fly_dirs("test.txt")
+        os.remove("test.txt")
+    
+    def test_error_wrong_format_long(self):  
+        """ _Test that the function raises a ValueError if the file list format is too long. """
+        with open("test.txt", "w") as file:
+            file.write("Fly_dir||Trial||task1||extra_input")
+        with self.assertRaises(ValueError):
+            read_fly_dirs("test.txt")
+        os.remove("test.txt")
+
+    def test_error_no_fly_file(self):
+        """ _Test that the function raises a FileNotFoundError if a fly directory does not exist. """
+        with open("test.txt", "w") as file:
+            file.write("Fake_file||Fake_trial||task1")
+        with self.assertRaises(FileNotFoundError):
+            read_fly_dirs("test.txt")
+        os.remove("test.txt")
+
+
 
 if __name__ == '__main__':
     unittest.main()
