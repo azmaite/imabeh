dc85e8fb0cbf1f681111d515cf86f77309c903fe
Maite Azcorra <azmaite@gmail.com>
2024-09-04 11:30:22 +0200

    Moved find_fictrac to fictrac

--- imabeh/behavior/behaviormain.py
+++ imabeh/behavior/behaviormain.py
@@ -12,7 +12,7 @@ import numpy as np
 from imabeh.general.main import find_file
 
 
-# FUNCTIONS TO FIND FILES
+# FUNCTIONS TO FIND GENERAL FILES
 
 def find_seven_camera_metadata_file(directory):
     """
@@ -33,32 +33,5 @@ def find_seven_camera_metadata_file(directory):
     return find_file(directory,
                       "capture_metadata.json",
                       "seven camera capture metadata")
-
-def find_fictrac_file(directory, camera=3, most_recent=False):
-    """
-    This function finds the path to the output file of fictrac of the form `camera_{cam}*.dat`, 
-    where `{cam}` is the values specified in the `camera` argument. 
-    If multiple files with this name are found and most_recent = False, it throws an exception.
-    otherwise, it returns the most recent file.
-
-    Parameters
-    ----------
-    directory : str
-        Directory in which to search.
-    camera : int
-        The camera used for fictrac.
-
-    Returns
-    -------
-    path : str
-        Path to fictrac output file.
-
-    """
-    return find_file(directory,
-                      f"camera_{camera}*.dat",
-                      "fictrac output",
-                      most_recent=most_recent)
-
-
 
 
