071024362c2495db6ea2ab55b376d6b19d637d94
Maite Azcorra <azmaite@gmail.com>
2025-04-03 16:14:42 +0200

    Added new df3d_video script. For now works only for 2d

--- imabeh/behavior/df3d.py
+++ imabeh/behavior/df3d.py
@@ -9,7 +9,7 @@ import pickle
 import numpy as np
 import pandas as pd
 import sys
-import datetime
+import cv2
 
 # IMPORT FUNCTIONS FROM df3d (deepfly3d package) and df3dPostProcessing
 from df3d.cli import main as df3dcli
@@ -21,7 +21,7 @@ from imabeh.run.userpaths import user_config, LOCAL_DIR
 from imabeh.general.main import find_file
    
 
-def run_df3d(trial_dir : str):
+def run_df3d(trial_dir : str, video = False):
     """run deepfly3d.
     To make it faster to run, the images are copied to a local folder and df3d is run on the local folder.
     The results are then moved to the correct output directory, and the local folder is deleted.
@@ -31,49 +31,41 @@ def run_df3d(trial_dir : str):
     trial_dir : str
         directory of the trial. should contain "behData/images" folder
         df3d will be saved within this trial folder as specified in the user_config
+    video : bool
+        whether to create a 3d video of the pose estimation
     """
 
     # Prepare the data for df3d by copying the images to the local data folder
     # this will make it faster to run df3d
-    _prepare_df3d(trial_dir)
+    # output is a local folder with the images
+    local_images_dir = _prepare_df3d(trial_dir)
 
     # Get the output_dir and camera_ids from user_config
     output_dir = user_config["df3d_path"]
     camera_ids = user_config["camera_order"]
-    # get the local folder where the images were copied to
-    images_dir = os.path.join(user_config["local_data"], *trial_dir.split(os.sep)[-3:], "images")
 
     # Simulate the command-line arguments
     output_dir_name = 'df3d'
-    sys.argv = [
-        "df3d-cli",         # The name of the command           
-        "-o", images_dir,  
-        "--output-folder", output_dir_name,  # Temporary folder to save the results (df3d cannot save outside of images_dir)
-        "--order", *map(str, camera_ids),
-    ]
+    if not video:
+        sys.argv = [
+            "df3d-cli",         # The name of the command           
+            "-o", local_images_dir,  
+            "--output-folder", output_dir_name,  # Temporary folder to save the results (df3d cannot save outside of images_dir)
+            "--order", *map(str, camera_ids),
+        ]
+    else:
+        sys.argv = [
+            "df3d-cli",         # The name of the command           
+            "-o", local_images_dir,  
+            "--output-folder", output_dir_name,  # Temporary folder to save the results (df3d cannot save outside of images_dir)
+            "--order", *map(str, camera_ids),
+            "--video-3d",               # Generate pose3d videos
+            "-n", "400"
+        ]
     # Call the df3d main function to run
     # MAKE SURE YOUR .bashrc FILE HAS "export CUDA_VISIBLE_DEVICES=0" 
     # OR THE GPU WONT BE USED AND DF3D WILL BE SLOW!!!!!
     df3dcli()
-    print('df3d done!')
-
-    # Make 3d-videos! only for first 150 frames (it's slow)
-    n = 150
-    # print time
-    print('\n making videos at time = ', datetime.datetime.now())
-    print(f"frames used = {n}")
-    sys.argv = [
-        "df3d-cli",         # The name of the command           
-        "-o", images_dir,  
-        "--output-folder", output_dir_name,  # Temporary folder to save the results (df3d cannot save outside of images_dir)
-        "--order", *map(str, camera_ids),
-        "--video-3d",               # Generate pose3d videos
-        "-n", str(n),               # Number of frames to generate videos for
-        "--skip-pose-estimation"    # Skip pose estimation (already done)
-    ]
-
-    df3dcli()
-    print('done making videos at time = ', datetime.datetime.now())
 
     # Move the output files to the correct output directory
     output_dir = os.path.join(trial_dir, output_dir)
@@ -81,19 +73,19 @@ def run_df3d(trial_dir : str):
         os.makedirs(output_dir)
     except:
         pass
-    output_dir_temp = os.path.join(images_dir, output_dir_name)
+    output_dir_temp = os.path.join(local_images_dir, output_dir_name)
     for file in os.listdir(output_dir_temp):
         os.system(f"mv {os.path.join(output_dir_temp, file)} {output_dir}")
     os.rmdir(output_dir_temp)
 
     # delete all jpgs
     print('deleting jpgs...')
-    for file in os.listdir(images_dir):
+    for file in os.listdir(local_images_dir):
         if file.endswith('.jpg'):
-            os.system('rm ' + os.path.join(images_dir, file))
+            os.system('rm ' + os.path.join(local_images_dir, file))
     
     # Delete the local data folder created to save the videos (with all the contents)
-    exp_dir = os.path.join(user_config["local_data"], *trial_dir.split(os.sep)[-3:-2])
+    exp_dir = os.path.join('/', *local_images_dir.split(os.sep)[:-2])
     os.system(f"rm -r {exp_dir}")
 
 
@@ -223,7 +215,6 @@ def get_df3d_df(trial_dir):
         pose = pickle.load(f)
     # get abdomen indeces from df3d_skeleton
     abdomen_keys = ["RStripe1", "RStripe2", "RStripe3", "LStripe1", "LStripe2", "LStripe3"]
-    abdomen_inds = [df3d_skeleton.index(key) for key in abdomen_keys]
     # add abdomen positions with proper names and number format to df3d_dict
     for i_abd, abd_key in enumerate(abdomen_keys):
         for i_xyz, xyz in enumerate(["x", "y", "z"]):
@@ -236,41 +227,149 @@ def get_df3d_df(trial_dir):
 
     # save the dataframe in the same folder as the df3d results as a pickle file
     df_out_dir = os.path.join(os.path.dirname(df3d_result), "df3d_df.pkl")
-    print(df_out_dir)
     df3d_df.to_pickle(df_out_dir)
     
     return df_out_dir
 
 
-def video_df3d(trial_dir):
-    """ Create a short video of the pose estimation results from df3d
-    superimposed on the original video."""
+def df3d_video(trial_dir : str, start_frame : int = 0, end_frame : int = 100):
+    """ make a video of the 2d and 3d pose estimation after PostProcessing.
 
-    # Prepare the data for df3d by copying the images to the local data folder
-    # this will make it faster to run df3d
-    _prepare_df3d(trial_dir)
+    Parameters
+    ----------
+    trial_dir : str
+        base directory where pose estimation results can be found
+    start_frame : int
+        starting frame for the video
+    end_frame : int
+        ending frame for the video
+    """
 
-    # Get the output_dir and camera_ids from user_config
-    output_dir = user_config["df3d_path"]
-    camera_ids = user_config["camera_order"]
-    # get the local folder where the images were copied to
-    images_dir = os.path.join(user_config["local_data"], *trial_dir.split(os.sep)[-3:], "images")
+    # get the df3d result file (after post-processing) - including 2d and 3d results
+    df3d_dir = trial_dir + '/behData/df3d'
+    df3d_result_path = find_df3d_file(df3d_dir, 'result', most_recent=True)
+    df3d_result = pickle.load(open(df3d_result_path, 'rb'))
+    points2d = df3d_result["points2d"]
 
-    # Simulate the command-line arguments
-    sys.argv = [
-        "df3d-cli",         # The name of the command           
-        "-o", images_dir,  
-        "--output-folder", 'df3d',  # Temporary folder to save the results (df3d cannot save outside of images_dir)
-        "--order", *map(str, camera_ids),
-        "--video-3d",               # Generate pose3d videos
-        "-n 150",                   # Number of frames to generate videos for
-    ]
+    # get the camera order from user_config and remove the front camera
+    cameras = [int(i) for i in user_config["camera_order"]]
+    cameras = cameras[::-1]
 
-    print("frames used = 150")
-    # Call the df3d main function to run
-    # MAKE SURE YOUR .bashrc FILE HAS "export CUDA_VISIBLE_DEVICES=0" 
-    # OR THE GPU WONT BE USED AND DF3D WILL BE SLOW!!!!!
-    df3dcli()
+    ## make the 2d videos by stacking videos for all left legs on top 
+    ## and right legs on the bottom and adding the 2d result points
+
+    # get the shape and hz of the videos
+    video_path = os.path.join(trial_dir, 'behData', 'images', 'camera_5.mp4')
+    cap = cv2.VideoCapture(video_path)
+    hz = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    cap.release()
+
+    # Output video setup
+    output_video_path = os.path.join(trial_dir, "behData", "df3d", f"df3d_video.mp4")
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_video_path, fourcc, hz/4, (width*3, height*2)) #4 times slowed down
+      
+    # set the colors for the left and right legs, and leg/abdomen names
+    legs_abd = ['F', 'M', 'H', 'Stripe'] # strip is for abdomen, 1, 2, 3
+    colors = {'L':[[15, 115, 153], [26, 141, 175], [117, 190, 203]], 'R':[[186, 30, 49], [201, 86, 79], [213, 133, 121]]} # LF, LM, LH, RF, RM, RH
+    leg_segments = ['Coxa', 'Femur', 'Tibia', 'Tarsus', 'Claw']
+    # convert all colors to BGR (OpenCV uses BGR)
+    for k in colors.keys():
+        for i in range(len(colors[k])):
+            colors[k][i] = colors[k][i][::-1]
+
+
+    # Get video snippets for each camera in order
+    videos = []
+    for camera_idx, camera_num in enumerate(cameras):
+        # skip the front camera
+        if camera_num == 3:
+            continue
+
+        # set the colors
+        if camera_idx < 3:
+            RL = 'L'
+        else:
+            RL = 'R'
+        colors_RL = colors[RL]
+
+        # get the video file
+        video_path = os.path.join(trial_dir, 'behData', 'images', f'camera_{camera_num}.mp4')
+        cap = cv2.VideoCapture(video_path)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
+        # add the frames from start to end_frame to videos list
+        frames = []
+        for frame_num in range(end_frame - start_frame):
+            _, frame = cap.read()
+
+            for leg_idx, leg in enumerate(legs_abd):
+
+                # get the leg points for each leg segment or abd
+                leg_points_idx = []
+                if leg == 'Stripe':
+                    segments = ['1', '2', '3']
+                else:
+                    segments = leg_segments
+
+                for segment in segments:
+                    # get the index for each leg segment
+                    leg_point_name = RL + leg + segment
+                    leg_point_idx = df3d_skeleton.index(leg_point_name)
+                    leg_points_idx.append(leg_point_idx)
+                # get the 2d points for each leg segment (x,y)
+                leg_points = points2d[camera_idx, start_frame+frame_num, leg_points_idx, :]
+                # get the color to plot
+                if leg == 'Stripe':
+                    color = [200, 200, 200]
+                else:
+                    color = colors_RL[leg_idx]
+
+                # Convert to absolute pixel coordinates
+                xy = np.transpose(np.vstack([leg_points[:, 1] * width, leg_points[:, 0] * height]))
+
+                # Ensure integer type and correct shape for OpenCV
+                xy = xy.astype(np.int32).reshape((-1, 1, 2))
+
+                # Draw the polyline for each leg/abd
+                cv2.polylines(frame, [xy], isClosed=False, color=color, thickness=7)
+                # Draw circles at each vertex
+                for point in xy:
+                    center = tuple(point[0])  # Extract (x, y) tuple
+                    cv2.circle(frame, center, radius=5, color=(128, 0, 0), thickness=5)  # Navy blue empty circles
+
+            # add frame to the list of frames for each video
+            frames.append(frame)
+
+        # add the camera frames to the videos list
+        videos.append(frames)
+
+        # release resources
+        cap.release()
+
+
+    # Create grid frame by frame
+    for t in range(end_frame - start_frame):
+        # stack the frames for each camera row by row
+        grid_frames = []
+        for j in range(2): # 2 rows
+            row_frames = [videos[j * 3 + k][t] for k in range(3)]
+            grid_frames.append(np.hstack(row_frames))
+        grid_frame = np.vstack(grid_frames)
+        
+        # save frame
+        out.write(grid_frame)
+
+    # Release resources
+    out.release()
+
+
+
+
+
+    
 
 
 
@@ -294,7 +393,9 @@ def _prepare_df3d(trial_dir : str):
     images_dir = os.path.join(trial_dir, "behData", "images")
 
     # create a temporary folder to save the images within local data folder
-    new_folder = os.path.join(*trial_dir.split(os.sep)[-3:])
+    new_folder = os.path.join(*images_dir.split(os.sep)[-3:-1])
     local_data_folder = os.path.join(local_data, new_folder)
     os.makedirs(local_data_folder, exist_ok=True)
     os.system(f"cp -r {images_dir} {local_data_folder}")
+    
+    return os.path.join(local_data_folder, 'images')
